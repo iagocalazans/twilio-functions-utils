f155a00070312c61792022502b263748552001bb
Iago Calazans <iago.calazans@gmail.com>
2022-08-18 19:49:03 -0300

    fix: exporting unauthorized error

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { useMock } = require('./lib/use.mock');
 const { BadRequestError } = require('./lib/errors/bad-request.error');
 const { InternalServerError } = require('./lib/errors/internal-server.error');
 const { NotFoundError } = require('./lib/errors/not-found.error');
+const { UnauthorizedError } = require('./lib/errors/unauthorized.error');
 const { Response } = require('./lib/responses/default.response');
 const { TwiMLResponse } = require('./lib/responses/twiml.response');
 
@@ -16,4 +17,5 @@ module.exports = {
   InternalServerError,
   NotFoundError,
   TwiMLResponse,
+  UnauthorizedError,
 };
