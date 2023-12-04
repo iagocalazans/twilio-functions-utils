cb644d559b26822078eed2bf27f210362a892793
Iago Calazans <iago.calazans@gmail.com>
2023-12-04 19:16:19 -0300

    chore(*): :art: combine functions utils and injector into only one
    TODO: Must update the README file to match the injector feats

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const { Try } = require('try2catch');
-const { useInjection } = require('./lib/use.injection');
-const { useMock } = require('./lib/use.mock');
 const { BadRequestError } = require('./lib/errors/bad-request.error');
 const { InternalServerError } = require('./lib/errors/internal-server.error');
 const { NotFoundError } = require('./lib/errors/not-found.error');
 const { UnauthorizedError } = require('./lib/errors/unauthorized.error');
 const { Response } = require('./lib/responses/default.response');
 const { TwiMLResponse } = require('./lib/responses/twiml.response');
+const {
+  useInjection,
+  useImports,
+  TWILIO_TYPES,
+  getFromRuntime,
+} = require('./lib/use.injection');
+const { typeOf } = require('./lib/typeof.function');
+const {
+  transformInstanceTo,
+  transformListTo,
+  pipe,
+  pipeAsync,
+} = require('./lib/transformers');
+const { extract, factory } = require('./lib/utils.function');
+const { useMock, useImportsMock } = require('./lib/use.mock');
+const { dispatch } = require('./lib/dispatch.function');
 
 module.exports = {
   useMock,
+  useImportsMock,
   useInjection,
+  dispatch,
+  extract,
+  factory,
+  pipe,
+  pipeAsync,
+  transformListTo,
+  transformInstanceTo,
+  typeOf,
+  useImports,
+  getFromRuntime,
   Response,
   BadRequestError,
   InternalServerError,
@@ -18,4 +43,5 @@ module.exports = {
   TwiMLResponse,
   UnauthorizedError,
   Try,
+  TWILIO_TYPES,
 };
